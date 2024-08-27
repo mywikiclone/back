d2c8bdc193ef02a398d83f46ef62deb6e754b8cb
dong3058 <hwangdonggeun70@gmail.com>
2024-08-27 23:33:16 +0900

    zzz

--- src/main/java/com/example/posttest/Config/WebConfig.java
+++ src/main/java/com/example/posttest/Config/WebConfig.java
@@ -25,8 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor(jwtUtil,redisTemplate))
                 .order(1)
-                .addPathPatterns("/update")
-                .excludePathPatterns("/**");
+                .addPathPatterns("/update");
 
 
 
