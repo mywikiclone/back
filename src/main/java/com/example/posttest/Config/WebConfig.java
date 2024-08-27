6718e686e63d646068af6e58877762d9c02b449a
dong3058 <hwangdonggeun70@gmail.com>
2024-08-27 22:26:00 +0900

    add options in cors config

--- src/main/java/com/example/posttest/Config/WebConfig.java
+++ src/main/java/com/example/posttest/Config/WebConfig.java
@@ -43,7 +43,7 @@ public class WebConfig implements WebMvcConfigurer {
 
         registry.addMapping("/**")
                 .allowedOriginPatterns("http://localhost:3000")
-                .allowedMethods("GET","POST")
+                .allowedMethods("GET","POST","OPTIONS")
                 .allowedHeaders("Authorization","Content-Type")//헤더도 이런설정이있따 ㅇㅇ;'
                 .allowCredentials(true);
     }
